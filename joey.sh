23e8e74644be6e415a248e9089bbf292dbd2bc1e
Maylay <maylay@users.noreply.github.com>
2024-05-21 09:45:58 -0400

    Initial commit
    Original script to flash the Joey Jr

---
+++ joey.sh
@@ -0,0 +1,68 @@
+
+for drives in 1 2 3 4 5 6 7 8 
+do
+	rdisk=`echo rdisk$drives`
+	dd if=/dev/$rdisk bs=512 skip=0 count=1 > check.txt 2> /dev/null
+	
+	if grep -q BENNVENN check.txt ; then
+		echo Joey Jr Found at /dev/$rdisk
+		
+		if [ $1 = "ROM" ]
+		then
+			echo Writing Rom... This may take minutes
+			dd if=$2 of=/dev/$rdisk bs=512 seek=37
+			break
+		fi
+		
+		if [ $1 = "FLASH" ]
+		then
+			echo Writing Flash Save... 
+			dd if=$2 of=/dev/$rdisk bs=512 seek=65573
+			break
+		fi
+		
+		if [ $1 = "SRAM" ]
+		then
+			echo Writing SRAM... 
+			dd if=$2 of=/dev/$rdisk bs=512 seek=65829
+			break
+		fi
+		
+		if [ $1 = "EEPROM" ]
+		then
+			echo Writing EEPROM... 
+			dd if=$2 of=/dev/$rdisk bs=512 seek=66085
+			break
+		fi
+		
+		if [ $1 = "ROMLIST" ] 
+		then
+			dd if=$2 of=/dev/$rdisk bs=512 seek=66341 
+			echo ROMLIST Update Complete
+			break
+		fi		
+
+		if [ $1 = "MODE" ] 
+		then
+			echo $2 > mode.txt
+			cnt=`echo $2 | wc -c`
+			let cnt=cnt-1
+			let cnt=512-cnt
+			head -c $cnt < /dev/zero >> mode.txt	
+			dd if=mode.txt of=/dev/$rdisk bs=512 seek=66725 2>/dev/null
+			echo Mode set to $2
+			sleep 1
+
+			if [ $2 = "UPDATE" ] 
+			then
+				dd if=$3 of=/dev/$rdisk bs=512 seek=66469 
+				echo Update Complete
+				break
+			fi	
+			break	
+		fi
+
+
+		break
+	fi
+done
